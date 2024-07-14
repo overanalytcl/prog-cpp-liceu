402a042d814e585304ec77d7e7260f2f251ca015
Ștefan Alecu <165364995+overanalytcl@users.noreply.github.com>
2024-07-15 01:46:36 +0300

    feat: add 1.13 code sample

---
+++ 01-rosu/01-elemente-de-baza/13-citiri-scrieri-cpp/13-citiri-scrieri-cpp/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int a;
+int main()
+{
+	int x;
+	cout << "x = "; cin >> x;
+	cout << "Valoarea lui x este " << x << '\n';
+	cout << "Valoarea lui a este " << a << '\n';
+	return 0;
+}
