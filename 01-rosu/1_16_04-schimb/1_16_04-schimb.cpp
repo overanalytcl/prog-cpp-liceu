8e06d6a8316951bc3bda215a6592e030b3e86c9e
Ștefan Alecu <165364995+overanalytcl@users.noreply.github.com>
2024-07-15 13:31:29 +0300

    feat: add 1.16.4 (Schimb)

---
+++ 01-rosu/1_16_04-schimb/1_16_04-schimb.cpp
@@ -0,0 +1,22 @@
+﻿/*
+	Schimb
+
+	Se introduc de la tastatură numerele reale a și b. Să se interschimbe 
+	valorile variabilelor a și b, apoi să se afișeze.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, aux;
+
+	cin >> a >> b;
+	aux = a;
+	a = b;
+	b = aux;
+
+	cout << a << " " << b << "\n";
+	return 0;
+}
