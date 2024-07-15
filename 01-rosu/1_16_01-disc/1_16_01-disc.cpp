b8324eb8e95793803b163a0b694098757875bc8b
Ștefan Alecu <165364995+overanalytcl@users.noreply.github.com>
2024-07-15 13:13:50 +0300

    feat: add 1.16.1 (Disc)

---
+++ 01-rosu/1_16_01-disc/1_16_01-disc.cpp
@@ -0,0 +1,23 @@
+﻿/*
+    Disc
+
+    Fie r un număr real, citit de la tastatură, care reprezintă lungimea razei 
+    unui cerc. Să se scrie un program care să calculeze și să se afișeze aria 
+    și perimetrul discului de rază r.
+*/
+
+#include <iostream>
+using namespace std;
+
+double PI = 3.14159265358979323846;
+
+int main()
+{
+    double r;
+    cin >> r;
+    double arie = PI * r * r;
+    double perimetru = 2 * PI * r;
+
+    cout << "Aria = " << arie << "\n" 
+        << "Perimetrul = " << perimetru << "\n";
+}
